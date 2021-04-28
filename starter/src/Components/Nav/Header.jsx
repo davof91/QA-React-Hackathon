6e547ffa62329331b230328a60e8da581ce6af23
Liz Linton <lizlinton@hotmail.com>
2021-04-28 13:29:19 +0100

    Header updated

--- starter/src/Components/Nav/Header.jsx
+++ starter/src/Components/Nav/Header.jsx
@@ -4,18 +4,15 @@ const Header = () => {
     return (
         <header className="container sticky-top" id="qa-navbar">
             <nav className="navbar navbar-expand-md navbar-light">
-            <a className="navbar-brand" href="https://qa.com"><img src="../images/qa.jpeg" alt="logo" /></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBarNav" aria-controls="navBarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item"><a href="/" class="nav-link">Home</a></li>
-                    <li className="nav-item"><a href="/schedule" class="nav-link">Schedule</a></li>
-                    <li className="nav-item"><a href="/signup" class="nav-link">Sign-up</a></li>
-                </ul>
-            </div>
-        </nav>
+                <a className="navbar-brand" href="/"><img src="../images/qa.jpeg" alt="logo" /></a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><a href="/" class="nav-link">Home</a></li>
+                        <li className="nav-item"><a href="/schedule" class="nav-link">Schedule</a></li>
+                        <li className="nav-item"><a href="/signup" class="nav-link">Sign-up</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     );
 }
