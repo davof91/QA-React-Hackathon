65d26946bcb72b4121f8f741f133f69f43c6f4ab
David Fernandez <davof91@gmail.com>
2021-04-29 09:00:18 +0100

    Adding axios query and error checking for the schedule

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState,useEffect }  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import 'popper.js';
@@ -9,15 +9,64 @@ import Home from './Components/Home/Home';
 import Schedule from './Components/Schedule/Schedule';
 import Header from './Components/Nav/Header';
 import Footer from './Components/Nav/Footer';
-import data from './data/cinemamockdata.json';
+//import data from './data/cinemamockdata.json';
 import axios from 'axios';
+
+const ALLFILMSURL = 'http://localhost:4000/allFilms';
+const OPENINGTIMESURL = 'http://localhost:4000/openingTimes';
 
 function App() {
   const [users, setNewUser] = useState([]);
   const [currentUser, setCurrentUser ] = useState("");
   const [currentPage, setPage] = useState("Home") 
-  const [movies, setMovies] = useState(data.allFilms);
-  const [openingTimes, setOpeningTimes] = useState(data.openingTimes);
+  const [movies, setMovies] = useState([]);
+  const [openingTimes, setOpeningTimes] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => { 
+      let movieData = await getFilms();
+      setMovies(movieData);
+    }
+
+    const getOpeningTimes = async () => { 
+      let ot = await getTimes();
+      console.log(ot)
+      setOpeningTimes(ot); 
+    }
+
+    getMovies(); 
+    getOpeningTimes();
+  }, []);
+
+  const getFilms = async () => {
+    try { 
+      const res = await axios.get(ALLFILMSURL); 
+      return res.data.length ? { 
+        movies: res.data 
+      } : {  
+        error: 'There are no films stored'
+      } ; 
+    } catch (e) { 
+      return { 
+        error: `Film data not available from server: ${e.message}` 
+      }; 
+    } 
+  };
+
+  const getTimes = async () => {
+    try { 
+      const res = await axios.get(OPENINGTIMESURL); 
+      return res.data.length ? { 
+        times: res.data 
+      } : {  
+        error: 'There are no films stored'
+      } ; 
+    } catch (e) { 
+      return { 
+        error: `Opening times data not available from server: ${e.message}`
+      }; 
+    } 
+  };
 
   const submitUser = (user) => {
     const updateUsers = [...users, user]
--- starter/src/Components/Schedule/Schedule.jsx
+++ starter/src/Components/Schedule/Schedule.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import Locations from '.../../../public/images/Locations.jpg';
 import CinemaTimes from './CinemaTimes'
 import Movies from './Movies';
+import { map } from 'jquery';
+import generateTodoId from '../utils/generateId'
 
 const Schedule = (props) => {
-    
+    let error = props.openingTimes.error? [props.openingTimes.error] : [];
+    error = error.concat(props.openingTimes.error? [props.movies.error] : error.length>0 ? error: [] )
+    console.log(error)
     return (
         <>
             <div className="container">
@@ -16,13 +20,24 @@ const Schedule = (props) => {
                 </a>
             </div>
 
-            <div className="container">
-                <CinemaTimes openingTimes={props.openingTimes}/>
-            </div>
-            
-            <div className="container">
-                <Movies movies={props.movies}/>
-            </div>
+            {error.length>0 ? (
+                    <>
+                        <h2>Oops, something went wrong!</h2>
+                        {error.map((e) => {let id = generateTodoId(); return <p key={id}>{e}</p>})}
+                        
+                    </>
+                ) : (
+                    <>
+                        <div className="container">
+                            <CinemaTimes openingTimes={props.openingTimes.times? props.openingTimes.times: []}/>
+                        </div>
+                        
+                        <div className="container">
+                            <Movies movies={props.movies.movies ? props.movies.movies : []}/>
+                        </div>
+                    </>
+                )
+            }   
         </>
     );
 }
